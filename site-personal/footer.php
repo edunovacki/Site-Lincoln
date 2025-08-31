45c23bd55ef5f7c5398575835d2e5e5dfccc9020
edunovacki <111133454+edunovacki@users.noreply.github.com>
2025-08-30 22:44:20 -0300

    Delete site-personal directory

--- site-personal/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-      <p>Desenvolvido Por Lincoln Tonolo - Todos os Direitos Reservados</p>
-      <div class="icones">
-      <a href="https://www.instagram.com/lincoln_tonolo/" target="_blank"><i class="fa-brands fa-instagram icones"></i></a>
-      <a href="https://wa.me/5544997686749" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
-    </div>
-    </footer>
